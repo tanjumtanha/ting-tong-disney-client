d7414bff38f6c1b79d36e894568bed66e799aa70
tanjumtanha <tanha.tanni27@gmail.com>
2023-05-21 11:48:57 +0600

    search done for all toys page

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table } from 'react-bootstrap';
-import { Link, useLoaderData } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const AllToys = () => {
-    const data = useLoaderData()
+    const [toys, setToys] = useState([]);
+    const [search, setSearch] = useState('');
+
+    const handleSearch = () => {
+        fetch(`http://localhost:5000/allToys?search=${encodeURIComponent(search)}`)
+            .then(res => res.json())
+            .then(data => {
+                setToys(data);
+            })
+            .catch(error => {
+                console.error('Error searching toys:', error);
+            });
+    };
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/allToys`)
+            .then(res => res.json())
+            .then(data => {
+                setToys(data);
+            })
+            .catch(error => {
+                console.error('Error fetching toys:', error);
+            });
+    }, []);
+
     return (
         <div>
-            <h2 className='text-center text-danger mb-4'>ALL THE TOYS</h2>
-            <div className='w-75 mx-auto table-responsive'>
-                <div className="search-box p-2 text-center">
+            <h2 className="text-center text-danger mb-4">ALL THE TOYS</h2>
+            <div className="w-75 mx-auto table-responsive">
+                <div className="search-box p-2 text-center mb-3">
                     <input
-                        // onChange={(e) => setSearchText(e.target.value)}
                         type="text"
-                        className="p-2"
-                    />{" "}
-                    <button className='btn btn-outline-danger'>Search</button>
+                        className="p-2 me-2"
+                        value={search}
+                        onChange={e => setSearch(e.target.value)}
+                    />
+                    <button className="btn btn-outline-danger" onClick={handleSearch}>
+                        Search
+                    </button>
                 </div>
                 <Table striped bordered hover>
                     <thead>
-                        <tr className='text-center'>
+                        <tr className="text-center">
                             <th>#</th>
                             <th>Toy Name</th>
                             <th>Seller</th>
@@ -29,7 +56,7 @@ const AllToys = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {data?.map((toy, index) => (
+                        {toys?.map((toy, index) => (
                             <tr key={toy._id}>
                                 <td>{index + 1}</td>
                                 <td>{toy.name}</td>
@@ -37,9 +64,11 @@ const AllToys = () => {
                                 <td>{toy.category}</td>
                                 <td>${toy.price}</td>
                                 <td>{toy.quantity}</td>
-                                <td><Link to={`/details/${toy._id}`} className="btn btn-danger">
-                                    View Details
-                                </Link></td>
+                                <td>
+                                    <Link to={`/details/${toy._id}`} className="btn btn-danger">
+                                        View Details
+                                    </Link>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
