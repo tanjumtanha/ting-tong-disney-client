ef88b68486319957faca998c60ba919582c91135
tanjumtanha <tanha.tanni27@gmail.com>
2023-05-21 07:35:29 +0600

    rating added

--- src/Pages/Home/Toys/CategoryWiseToy/CategoryWiseToy.jsx
+++ src/Pages/Home/Toys/CategoryWiseToy/CategoryWiseToy.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 import { motion } from 'framer-motion';
+import { FaStar, FaStarHalfAlt } from 'react-icons/fa';
 
 const CategoryWiseToy = ({ toy }) => {
   const { _id, picture, name, price, rating } = toy || {};
@@ -13,6 +14,25 @@ const CategoryWiseToy = ({ toy }) => {
   const imageVariants = {
     hover: { scale: 1.1 },
   };
+
+  const renderRating = () => {
+    const filledStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 !== 0;
+    const stars = [];
+
+    for (let i = 0; i < 5; i++) {
+      if (i < filledStars) {
+        stars.push(<FaStar key={i} color="gold" />);
+      } else if (hasHalfStar && i === filledStars) {
+        stars.push(<FaStarHalfAlt key={i} color="gold" />);
+      } else {
+        stars.push(<FaStar key={i} color="lightgray" />);
+      }
+    }
+
+    return stars;
+  };
+
   return (
     <motion.div
       variants={cardVariants}
@@ -21,20 +41,23 @@ const CategoryWiseToy = ({ toy }) => {
       whileHover="hover"
       className="col-lg-4 col-md-6 col-sm-12"
     >
-      <Card className="mb-4" style={{backgroundColor:"lightpink"}}>
+      <Card className="mb-4" style={{ backgroundColor: "lightpink" }}>
         <motion.img
           src={picture}
           alt={name}
           variants={imageVariants}
           className="card-img-top img-fluid"
-          style={{height:"300px" }}
+          style={{ height: "350px" }}
         />
         <Card.Body>
           <Card.Title>{name}</Card.Title>
           <Card.Text>
             <strong>Price:</strong> ${price}
             <br />
-            <strong>Rating:</strong> {rating}
+            <div className='d-flex align-items-center'>
+              {renderRating()}
+              <span className='ms-1'>{rating}</span>
+            </div>
           </Card.Text>
           <Link to={`/products/${_id}`} className="btn btn-danger">
             View Details
